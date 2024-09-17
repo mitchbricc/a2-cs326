941cfaeaa599fbafa68da0ac9f56a32241debac4
Mitchell Bricco <briccm84@uwosh.edu>
2024-09-17 12:52:33 -0500

    added name

--- AES.java
+++ AES.java
@@ -1,6 +1,6 @@
 /*****************************************************
    CS 326 - Fall 2024 - Assignment #2
-   Full name of student #1: ____________________
+   Full name of student #1: Mitchell Bricco
    Full name of student #2: ____________________
    Full name of student #3: ____________________
    (delete the line above this one if your team is a pair)

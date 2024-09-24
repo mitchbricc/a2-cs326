9764c4264b6cb2e832c990a71f76ab829ef4749f
max <max@mpatterson.xyz>
2024-09-24 15:18:55 -0500

    Add print state, mixColumns and inverseMixColumns test

--- main.java
+++ main.java
@@ -7,24 +7,22 @@ import java.math.BigInteger;
 public class main {
 
     public static void main(String[] args) {
-        encryptTest();
-        // int[][] state = {
-        //     {219, 0x13, 0x53, 0x45},
-        //     {19, 0x0a, 0x22, 0x5c},
-        //     {83, 0x01, 0x01, 0x01},
-        //     {69, 0xc6, 0xc6, 0xc6},
-        // };
+        // encryptTest();
+        int[][] state = {
+                { 1, 0x13, 0x53, 0x45 },
+                { 2, 0x0a, 0x22, 0x5c },
+                { 3, 0x01, 0x01, 0x01 },
+                { 4, 0xc6, 0xc6, 0xc6 },
+        };
 
-        // AES.mixColumns(state);
+        AES.mixColumns(state);
+        printState(state);
 
-        // for (int i = 0; i < state.length; i++) {
-        //     for (int j = 0; j < state[i].length; j++) {
-        //         System.out.print(state[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
+        AES.inverseMixColumns(state);
+        printState(state);
     }
-    public static String textToHex(String plaintext){
+
+    public static String textToHex(String plaintext) {
         byte[] bytes = plaintext.getBytes();
         String plainHex = "";
         for (int i = 0; i < 8; i++) {
@@ -32,7 +30,8 @@ public class main {
         }
         return plainHex;
     }
-    public static String hexToString(String plainHex){
+
+    public static String hexToString(String plainHex) {
         StringBuilder result = new StringBuilder();
         // Split the hex string into pairs of characters
         for (int i = 0; i < plainHex.length(); i += 2) {
@@ -44,26 +43,36 @@ public class main {
         }
         return result.toString();
     }
-    public static boolean encryptTest(){
+
+    public static void printState(int[][] s) {
+        for (int i = 0; i < s.length; i++) {
+            for (int k = 0; k < s[i].length; k++) {
+                System.out.print(s[i][k] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean encryptTest() {
         String plaintext = "helloworld this is a sentence of unknown length. I hope it is long enough";
         byte[] bytes = plaintext.getBytes();
         String plainHex = "";
         for (int i = 0; i < 8; i++) {
             plainHex += String.format("%02X", bytes[i]);
         }
-        System.out.println("plaintext: "+plainHex);
+        System.out.println("plaintext: " + plainHex);
         String key = "this is a key that is useless";
         byte[] bkey = key.getBytes();
         String keyHex = "";
         for (int i = 0; i < 8; i++) {
             keyHex += String.format("%02X", bkey[i]);
         }
-        System.out.println("keyHex: "+keyHex);
-        int[][] c = AES.encrypt(plainHex,keyHex);
+        System.out.println("keyHex: " + keyHex);
+        int[][] c = AES.encrypt(plainHex, keyHex);
         AES.printMatrix(c);
 
         StringBuilder result = new StringBuilder();
-        
+
         // Split the hex string into pairs of characters
         for (int i = 0; i < plainHex.length(); i += 2) {
             // Get the substring of 2 characters

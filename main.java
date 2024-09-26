fa39f4bbe969e0ec4defefe3a6741ad8c893a2c0
Mitchell Bricco <briccm84@uwosh.edu>
2024-09-26 15:45:44 -0500

    tests

--- main.java
+++ main.java
@@ -9,12 +9,13 @@ import java.util.*;
 public class main {
 
     public static void main(String[] args) {
+        addRoundKeyTest();
         // int n = 5;
         // System.out.println("2x"+n);
         // System.out.println("times2: " +AES.times2(n));
         // System.out.println("times: "+AES.times(n,2));
         // System.out.println("times: "+AES.times(2,n));
-        encryptTest();
+        //encryptTest();
         // int[][] state = {
         //     {219, 0x13, 0x53, 0x45},
         //     {19, 0x0a, 0x22, 0x5c},
@@ -55,26 +56,7 @@ public class main {
             }
         }
     }
-    public static String textToHex(String plaintext){
-        byte[] bytes = plaintext.getBytes();
-        String plainHex = "";
-        for (int i = 0; i < 8; i++) {
-            plainHex += String.format("%02X", bytes[i]);
-        }
-        return plainHex;
-    }
-    public static String hexToString(String plainHex){
-        StringBuilder result = new StringBuilder();
-        // Split the hex string into pairs of characters
-        for (int i = 0; i < plainHex.length(); i += 2) {
-            // Get the substring of 2 characters
-            String byteString = plainHex.substring(i, i + 2);
-            // Convert the hex pair to an integer
-            int charCode = Integer.parseInt(byteString, 16);
-            result.append((char) charCode);
-        }
-        return result.toString();
-    }
+    
     public static void inverseMixColumnsTest(){
         System.out.println("mixColumnsTest");
         int[][] m = new int[4][4];
@@ -110,6 +92,50 @@ public class main {
         System.out.println("mix");
         AES.printMatrix(m);
     }
+
+    public static void inverseSubstituteBytesTest(){
+        System.out.println("inverseSubstituteBytesTest");
+        int[][] m = new int[4][4];
+        for (int i = 0; i < 16; i++) {
+            m[i%4][i/4] = i;
+        }
+        AES.printMatrix(m);
+        AES.forwardSubstituteBytes(m);
+        AES.inverseSubstituteBytes(m);
+        System.out.println("output should match");
+        AES.printMatrix(m);
+    }
+
+    public static void shiftRowsTest(){
+        System.out.println("shiftrowsTest");
+        String s = "11213141122232421323334314243444";
+        int[][] m = AES.hexStringToByteArray(s);
+        AES.printMatrix(m);
+        AES.shiftRows(m);
+        System.out.println("output should match");
+        String o = "11223344122334411324314214213243";
+        int[][] state = AES.hexStringToByteArray(o);
+        AES.printMatrix(state);
+        System.out.println("output");
+        AES.printMatrix(m);
+    }
+     public static void addRoundKeyTest(){
+        System.out.println("addRoundKeyTest");
+        int[][] m = new int[4][4];
+        for (int i = 0; i < 16; i++) {
+            m[i%4][i/4] = i/4;
+        }
+        AES.printMatrix(m);
+        int[][] k = AES.hexStringToByteArray("01020304020406080001020301020304");
+        int[] w = AES.expandKey(k);
+        System.out.println("last word of key");
+        for (int j = 0; j < 4; j++) {
+            System.out.printf("%02x ".toUpperCase(), w[3]>>8*(3-j)&0xff);
+        }
+        AES.addRoundKey(m, w, 1);
+        System.out.println("\noutput");
+        AES.printMatrix(m);
+     }
 
     public static void encryptTest(){
         String plaintext = "0123456789abcdeffedcba9876543210";
@@ -149,5 +175,26 @@ public class main {
         }
         System.out.println(result);
         return false;
+    }
+
+    public static String textToHex(String plaintext){
+        byte[] bytes = plaintext.getBytes();
+        String plainHex = "";
+        for (int i = 0; i < 8; i++) {
+            plainHex += String.format("%02X", bytes[i]);
+        }
+        return plainHex;
+    }
+    public static String hexToString(String plainHex){
+        StringBuilder result = new StringBuilder();
+        // Split the hex string into pairs of characters
+        for (int i = 0; i < plainHex.length(); i += 2) {
+            // Get the substring of 2 characters
+            String byteString = plainHex.substring(i, i + 2);
+            // Convert the hex pair to an integer
+            int charCode = Integer.parseInt(byteString, 16);
+            result.append((char) charCode);
+        }
+        return result.toString();
     }
 }
